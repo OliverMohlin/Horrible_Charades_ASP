b167f572ed7b5fd70dde1622c4968683ad901351
Administrator <Administrator@ACADEMY-6162X40>
2016-08-23 10:32:02 +0200

    Add project files.

---
+++ Horrible_Charades_ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Horrible_Charades_ASP.MvcApplication" Language="C#" %>
---
+++ Horrible_Charades_ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Horrible_Charades_ASP.Startup))]
+namespace Horrible_Charades_ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
