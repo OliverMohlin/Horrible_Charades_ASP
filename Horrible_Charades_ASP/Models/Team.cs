3dffc833f19113d00c708ca96079d70ff22d7664
Administrator <Administrator@ACADEMY-6162X40>
2016-08-23 14:00:42 +0200

    Team och Charade-klass

---
+++ Horrible_Charades_ASP/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Horrible_Charades_ASP.Models
+{
+    public class Team
+    {
+        public Team(string name, string hash)
+        {
+            Name = name;
+            Hash = hash;
+            Id = Guid.NewGuid().ToString("d");
+        }
+        public string ConnectionID { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public string Group { get; set; }
+        public bool isPlaying { get; set; }
+    }
+}
