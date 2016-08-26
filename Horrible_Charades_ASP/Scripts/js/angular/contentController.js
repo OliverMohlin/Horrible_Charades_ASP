ace156546f862f4a8ee2340f7bd3c65f601cfea6
Toni Lee <contact.tonilee@gmail.com>
2016-08-26 11:20:42 +0200

    Kommenterat vår js-kod

--- Horrible_Charades_ASP/Scripts/js/angular/contentController.js
+++ Horrible_Charades_ASP/Scripts/js/angular/contentController.js
@@ -10,29 +10,39 @@
         var hub = $.connection.gameHub; //Saves connection in "hub"-variable
         console.log(gameService);
         vm.Team = {};
+
+        //Calls CreateGame function on Server-Side when CreateTeamHost is loaded
         vm.createGame = function () {
             hub.server.createGame();
         };
+
+        //Calls JoinGame function on Server-Side when a teamName and GameCode is submitted in CreateTeamGuest
         vm.joinGame = function () {
             gameService.gameCode = $("#GameCode").val();
             alert("Join Game in contentCtrl");
             console.log(gameService);
             hub.server.joinGame(vm.Team.Name, gameService.gameCode);
         };
+
+        //Calls CreateTeam function on Server-Side when a teamName in CreateTeamHost is submitted
         vm.createTeam = function () {
             gameService.gameCode = $("#GameCode").text();
             console.log(gameService.gameCode);
             hub.server.createTeam(vm.Team.Name, gameService.gameCode);
         };
+
+        //Calls GetCharade function on Server-Side when PreCharadeActor is loaded
         vm.getCharade = function () {
             console.log(vm.Team);
             hub.server.getCharade(gameService.gameCode);
         };
-
+        
+        //Calls UpdateCharade function on Server-Side when "Get Adjective"-button is pressed
         vm.getAdjective = function () {
             hub.server.updateCharade("adjective", gameService.gameCode);
         };
 
+        //Calls UpdateCharade function on Server-Side when "Get Verb"-button is pressed
         vm.getVerb = function () {
             hub.server.updateCharade("verb", gameService.gameCode);
         };
--- Horrible_Charades_ASP/Scripts/temp.js
+++ Horrible_Charades_ASP/Scripts/temp.js
@@ -4,6 +4,8 @@
         $("#result").append("<li>" + textToWrite + "</li>");
     };
 
+    //Write out TeamName, ConnectionId and connected users in CreateTeamHost
+    //Todo: If successful redirect user to lobby
     hub.client.teamsJoined = function (game) {
         console.log(game);
         $("#teams").append("TeamName: " + game.Teams[0].Name + "<br /> ConnectionId: " + game.Teams[0].ConnectionID + "<br /> ConnectedClients: " + game.Teams.length + "<br /> <br />");
@@ -15,6 +17,7 @@
 
     //};
 
+    //Write out and append new words to a charade in Pre-Charade(?)
     hub.client.InsertCharadeHTML = function (charadeWord, typeOfWord) {
         if (typeOfWord === "noun") {
             console.log("printing noun");
@@ -37,20 +40,23 @@
 
     };
 
+    //Write out the GameCode in CreateTeamHost
     hub.client.printGameCode = function (game) {
         console.log(game);
         //vm.Team.GameCode = game.GameCode
         $("#GameCode").append(game.GameCode);
     };
 
+    //Adds a Noun to a charade(?)
     $("#getNounButton").click(function () {
         hub.server.getNoun();
     });
 
+    //Adds an adjective to a charade
     $("#getAdjectiveButton").click(function () {
         hub.server.getAdjective();
     });
-
+    //Adds a verb to a charade
     $("#getVerbButton").click(function () {
         hub.server.getVerb();
     });
